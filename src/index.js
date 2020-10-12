a004c1d76240fb1e322d44f79e947e596c00353d
Nastassia Zharnasek <Nastassia_Zharnasek@epam.com>
2020-10-12 22:13:29 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -1,46 +1,65 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    // write your solution here
+  let result = "";
+  expr.split("**********").forEach((word) => {
+    let letters = "";
+    for (let i = 0; i <= word.length - 10; i = i + 10) {
+      let decodedLetter = word.substring(i, i + 10);
+      decodedLetter = decodedLetter.substring(decodedLetter.indexOf("1"));
+
+      let morseLetter = "";
+      for (let j = 0; j <= decodedLetter.length - 2; j += 2) {
+        morseLetter = morseLetter.concat(
+          decodedLetter.substring(j, j + 2) === "11" ? "-" : "."
+        );
+      }
+     
+      letters = letters.concat(MORSE_TABLE[morseLetter]);
+    }
+    result = result.concat(letters + " ");
+  });
+  
+  return result.trim();
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
